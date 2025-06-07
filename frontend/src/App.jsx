7b03fa578d99ebe28f335081a39c21f897fdf5fd
sachin patware <sachinpatware10@gmail.com>
2025-06-08 00:48:27 +0530

    add search ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,24 +11,25 @@ import Signup from "./Components/signup";
 import Login from "./Components/login";
 import Logout from "./Components/logout";
 import UserProfile from "./Components/userprofile";
-import "./Components/rating.css"
+import "./Components/rating.css";
 import "./App.css";
 import { FaRegCompass } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import { ToastContainer } from "react-toastify";
 
 function App() {
   const [user, setUser] = useState(null);
-  const[email,setEmail]=useState(null);
+  const [email, setEmail] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const res = await axios.get("/user");
         setUser(res.data.user);
-        setEmail(res.data.email)
+        setEmail(res.data.email);
       } catch {
         setUser(null);
-        setEmail(null)
+        setEmail(null);
       }
     };
 
@@ -56,20 +57,41 @@ function App() {
                 <Link className="nav-link " to="/">
                   Explore
                 </Link>
-      
               </div>
+              <div class="navbar-nav ms-auto">
+                  <form class="d-flex " role="search">
+                    <input
+                      class="form-control input_search me-2"
+                      type="search"
+                      placeholder="Search destination"
+                      aria-label="Search"
+                    />
+                    <button class="btn " id="btn_search" type="submit">
+                     <FaMagnifyingGlass /> Search 
+                    </button>
+                  </form>
+                </div>
               <div className="navbar-nav ms-auto gap-2 ">
-                <Link className="nav-link d-flex align-items-center " to="/listing/new">
+                <Link
+                  className="nav-link d-flex align-items-center "
+                  to="/listing/new"
+                >
                   <span>Become a Host</span>
                 </Link>
                 {user ? (
-                  <UserProfile user={user} email={email}/>
+                  <UserProfile user={user} email={email} />
                 ) : (
                   <>
-                    <Link className="nav-link  fw-semibold text-dark" to="/signup">
-                     SignUp
+                    <Link
+                      className="nav-link  fw-semibold text-dark"
+                      to="/signup"
+                    >
+                      SignUp
                     </Link>
-                    <Link className="nav-link  fw-semibold text-dark" to="/login">
+                    <Link
+                      className="nav-link  fw-semibold text-dark"
+                      to="/login"
+                    >
                       LogIn
                     </Link>
                   </>
@@ -82,9 +104,18 @@ function App() {
         <ToastContainer />
 
         <Routes>
-          <Route path="/signup" element={<Signup setUser={setUser} setEmail={setEmail} />} />
-          <Route path="/login" element={<Login setUser={setUser} setEmail={setEmail}/>} />
-          <Route path="/logout" element={<Logout setUser={setUser} setEmail={setEmail}/>} />
+          <Route
+            path="/signup"
+            element={<Signup setUser={setUser} setEmail={setEmail} />}
+          />
+          <Route
+            path="/login"
+            element={<Login setUser={setUser} setEmail={setEmail} />}
+          />
+          <Route
+            path="/logout"
+            element={<Logout setUser={setUser} setEmail={setEmail} />}
+          />
           <Route path="/" element={<ListingPage />} />
           <Route path="/listing/edit" element={<EditListing />} />
           <Route path="/listing/new" element={<NewListing />} />
