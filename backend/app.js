462971609fcef657ea197f17530a96112e78c448
sachin patware <sachinpatware10@gmail.com>
2025-06-08 20:43:07 +0530

    update session cookie settings to enable secure option in production

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ const sessionOptions = {
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production" ? true : false,
     sameSite: process.env.NODE_ENV === "production" ? "None" : undefined,
     expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
     maxAge: 1 * 24 * 60 * 60 * 1000,
