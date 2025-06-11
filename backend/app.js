7ddb11e0d3d96eaf70f01fc7708a5fcaaa82e262
sachin patware <sachinpatware10@gmail.com>
2025-06-11 11:33:51 +0530

    fix cookie problem

--- backend/app.js
+++ backend/app.js
@@ -17,58 +17,10 @@ const methodOverride = require("method-override");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-// database connection');
 
-// const mongo_url = "mongodb://127.0.0.1:27017/air_travles";
-const mongo_url = process.env.ATLAS_URL;
-async function connection() {
-  await mongoose.connect(mongo_url);
-}
-connection()
-  .then(() => console.log("Connected!"))
-  .catch((err) => {
-    console.log(err);
-  });
 
-// session store
-const store = MongoStore.create({
-  mongoUrl: mongo_url,
-  crypto: {
-    secret: "MySecretCode",
-  },
-  touchAfter: 24 * 3600,
-});
-store.on("error", (e) => {
-  console.log("Session store error", e);
-});
-const sessionOptions = {
-  store: store,
-  secret: "MySecretCode",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: process.env.NODE_ENV === "production" ? "None" : undefined,
-    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
-    maxAge: 1 * 24 * 60 * 60 * 1000,
-  },
-};
+app.set('trust proxy', 1);
 
-app.use(methodOverride("X-HTTP-Method-Override"));
-app.use(session(sessionOptions));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new localStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "public")));
-
-let port = process.env.PORT || 5555;
 
 app.use(
   cors({
@@ -81,23 +33,69 @@ app.use(
   })
 );
 
-// Routes
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(methodOverride("X-HTTP-Method-Override"));
+
+const mongo_url = process.env.ATLAS_URL;
+const store = MongoStore.create({
+  mongoUrl: mongo_url,
+  crypto: { secret: "MySecretCode" },
+  touchAfter: 24 * 3600,
+});
+store.on("error", (e) => console.log("Session store error", e));
+
+const sessionOptions = {
+  store: store,
+  secret: "MySecretCode",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : undefined,
+    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  },
+};
+
+app.use(session(sessionOptions));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use("/", userRouter);
 app.use("/listing", listingRouter);
-
 app.use("/listing/:id/reviews", reviewRouter);
+
 
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found 😕"));
 });
 
 app.use((err, req, res, next) => {
-  let { statusCode = 500, message = " try again Somthing went Wrong 😕!" } =
-    err;
+  let { statusCode = 500, message = "Try again, something went wrong 😕!" } = err;
   res.status(statusCode).send(message);
 });
 
+
+mongoose
+  .connect(mongo_url)
+  .then(() => console.log("Connected to DB!"))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5555;
 app.listen(port, () => {
-  console.log(`server at http://172.20.10.2:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
